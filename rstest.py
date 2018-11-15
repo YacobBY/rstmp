8b9b7086fe69f01fb850e87f5a6d52afdc3cd961
Yacob <yacob101@gmail.com>
2018-11-15 16:07:58 +0100

    Update rstest.py

--- rstest.py
+++ rstest.py
@@ -1,25 +1,39 @@
 import pandas as pd
-# match = 'item'
-# url = 'http://oldschoolrunescape.wikia.com/wiki/Brian%27s_Archery_Supplies'
-#
-# dfs =  pd.read_html(url, match=match)
-#
-# print(dfs)
-
 from bs4 import BeautifulSoup
-import urllib3.request
 import requests
 
-wiki = "http://oldschoolrunescape.wikia.com/wiki/Davon%27s_Amulet_Store"
-# header = {'User-Agent': 'Mozilla/5.0'}  # Needed to prevent 403 error on Wikipedia`enter code here`
-# req = urllib3.request(wiki, headers=header)
-# page = urllib3.urlopen(wiki)
+wiki = "http://oldschoolrunescape.wikia.com/wiki/Category:Shops?from=*"
+
+wikiText = requests.get(wiki)
+soup = BeautifulSoup(wikiText.text, features="lxml")
+
+links = [a.get('href') for a in soup.find_all('a', href=True)]
+for link in links:
+
+    print(link)
+
+
+
+
+# get a table
 url = "http://oldschoolrunescape.wikia.com/wiki/Davon%27s_Amulet_Store"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, features="lxml")
-
-
 table = soup.find("table", {"class": "wikitable sortable"})
-df = pd.read_html(str(table))
+dft = pd.read_html(str(table))
+print(links)
+df = dft[0]
 print(df)
 
+print("aAaaaa")
+
+url = "http://oldschoolrunescape.wikia.com/wiki/Bob%27s_Brilliant_Axes"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, features="lxml")
+table = soup.find("table", {"class": "wikitable sortable"})
+dfTemp2 = pd.read_html(str(table))
+dfTemp = dfTemp2[0]
+print(dfTemp)
+
+df_new = pd.concat([df, dfTemp])
+print(df_new)
